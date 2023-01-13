23f12b78ee5883b7f9da19662a0d0f5c1579a406
Roman <romabogdan147@gmail.com>
2023-01-13 21:07:14 +0300

    feat: add slider functional (date)

--- js/index.js
+++ js/index.js
@@ -1,7 +1,39 @@
+import Slider  from "./Slider.js"
+
+
+
 const burgerButton = document.querySelector('.header__burger')
 const headerNavigation = document.querySelector('.header__nav')
+
+const images = document.querySelectorAll(".slider__img")
+const sliderLine = document.querySelector('.slider__line')
+const nextSlide = document.querySelector('.slider__next')
+const prevSlide = document.querySelector('.slider__prev')
+const sliderItems = document.querySelectorAll('.sliderItem__link')
+const sliderContainer = document.querySelector(".projects__slider")
 
 burgerButton.addEventListener('click',()=>{
   burgerButton.classList.toggle('header__burger--open')
   headerNavigation.classList.toggle('header__nav--open')
 })
+
+const slider = new Slider({
+  sliderLine:  sliderLine,
+  sliderItems: sliderItems,
+  nextSlide: nextSlide,
+  prevSlide: prevSlide,
+  images: images,
+  sliderContainer: sliderContainer
+})
+slider.init()
+
+window.addEventListener('resize', slider.init )
+
+
+
+
+
+
+
+
+
